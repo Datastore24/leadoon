7e0038453ffaeb518fe0871e10db5a52a745f97b
Viktor Mishustin <mrgyugo@gmail.com>
2015-10-15 00:25:10 +0300

    Разработан интерфейс таблицы заказов

---
+++ Leadoon/ScoreboardOrdersView.m
@@ -0,0 +1,13 @@
+//
+//  ScoreboardOrdersView.m
+//  Leadoon
+//
+//  Created by Viktor on 14.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import "ScoreboardOrdersView.h"
+
+@implementation ScoreboardOrdersView
+
+@end
