44abac3495fdd010925161cefa3eb3d0e887db24
Viktor Mishustin <mrgyugo@gmail.com>
2015-10-15 21:16:56 +0300

    Разработка интерфейса ячейки в Табло Заказов

--- Leadoon/LabelsTableViewCall.h
+++ Leadoon/LabelsTableViewCall.h
@@ -7,7 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface LabelsTableViewCall : NSObject
+
+//Тип заказа----------------------------------------------------
+- (UILabel*) labelTypeTableViewCell: (NSString*) latebType;
+
+//Картинка типа заказа------------------------------------------
+- (UIImageView*) imageViewTypeTableView;
+
+//Станция метро-------------------------------------------------
+- (UILabel *) labelMetroStationName: (NSString*) stationName;
+
+//Изображение тележки---------------------------------------------
+- (UIImageView *) imageViewBasketTableView;
+
+//Вес, кол-во заказов---------------------------------------------
+- (UILabel*)weightAndNumberOfOrders:(NSString*)stringOrders;
+
+//Дней осталось---------------------------------------------------
+- (UILabel*) labelDaysLeft: (NSString*) stringDays;
+
+//Временной интервал-----------------------------------------------
+- (UILabel*)labelTimeInterval:(NSString*)strigInterval;
+
+//Осталось не изменяемый label-------------------------------------
+- (UILabel *) labelLineLeft;
+
+//Оставшееся время выполнения заказа--------------------------------
+- (UILabel*) labelTimeRemaining: (NSString *) remainingString;
+
+//Формирование заказа----------------------------------------------
+- (UILabel*)labelFormation:(NSString*)stringFormation;
 
 @end
--- Leadoon/LabelsTableViewCall.m
+++ Leadoon/LabelsTableViewCall.m
@@ -7,7 +7,165 @@
 //
 
 #import "LabelsTableViewCall.h"
+#import "UIColor+HexColor.h"
+
+@interface LabelsTableViewCall ()
+
+@property (strong, nonatomic) UILabel* typeLabel;
+
+@end
 
 @implementation LabelsTableViewCall
+
+//Тип заказа----------------------------------------------------
+- (UILabel*)labelTypeTableViewCell:(NSString*)latebType
+{
+
+    self.typeLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.f, 10.f, 100.f, 20.f)];
+    self.typeLabel.text = latebType;
+    self.typeLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:20];
+
+    NSString* blueString = @"Заказ";
+    NSString* greenString = @"Забор";
+    NSString* brownString = @"Закупка";
+
+    if (self.typeLabel.text == blueString) {
+
+        self.typeLabel.textColor = [UIColor colorWithHexString:@"0e59db"];
+    }
+
+    else if (self.typeLabel.text == greenString) {
+
+        self.typeLabel.textColor = [UIColor colorWithHexString:@"2c6530"];
+    }
+
+    else if (self.typeLabel.text == brownString) {
+
+        self.typeLabel.textColor = [UIColor colorWithHexString:@"cc8023"];
+    }
+
+    else {
+        self.typeLabel.textColor = [UIColor blackColor];
+    }
+
+    return self.typeLabel;
+}
+
+//Картинка типа lableTableViewCell-------------------------------------------
+
+- (UIImageView*)imageViewTypeTableView
+{
+    NSString* image1String = @"Заказ";
+    NSString* image2String = @"Закупка";
+
+    NSString* nameImage;
+
+    if (self.typeLabel.text == image1String || self.typeLabel.text == image2String) {
+
+        nameImage = @"imageMetro1.png";
+    }
+
+    else {
+        nameImage = @"imageMetro2.png";
+    }
+
+    UIImage* image = [UIImage imageNamed:nameImage];
+
+    UIImageView* imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 55, 20, 20)];
+    imageView.image = image;
+
+    return imageView;
+}
+
+//Станция метро-------------------------------------------------
+- (UILabel*)labelMetroStationName:(NSString*)stationName
+{
+
+    UILabel* labelMetro = [[UILabel alloc] initWithFrame:CGRectMake(35, 55, 100, 20)];
+
+    labelMetro.text = stationName;
+    labelMetro.font = [UIFont fontWithName:@"HelveticaNeue" size:12];
+
+    return labelMetro;
+}
+
+//Изображение тележки---------------------------------------------
+- (UIImageView*)imageViewBasketTableView
+{
+
+    NSString* imageName = @"basket.png";
+    UIImage* imageBusket = [UIImage imageNamed:imageName];
+
+    UIImageView* imageViewBusket = [[UIImageView alloc] initWithFrame:CGRectMake(120, 55, 20, 20)];
+    imageViewBusket.image = imageBusket;
+
+    return imageViewBusket;
+}
+
+//Вес, кол-во заказов---------------------------------------------
+- (UILabel*)weightAndNumberOfOrders:(NSString*)stringOrders
+{
+    UILabel* labelWeightAndNumbers = [[UILabel alloc] initWithFrame:CGRectMake(150, 55, 60, 20)];
+    labelWeightAndNumbers.text = stringOrders;
+    labelWeightAndNumbers.font = [UIFont fontWithName:@"HelveticaNeue" size:12];
+
+    return labelWeightAndNumbers;
+}
+
+//Дней осталось---------------------------------------------------
+- (UILabel*)labelDaysLeft:(NSString*)stringDays
+{
+    UILabel* labelDays = [[UILabel alloc] initWithFrame:CGRectMake(130, 5, 78, 15)];
+    labelDays.text = stringDays;
+    labelDays.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];
+    labelDays.textColor = [UIColor colorWithHexString:@"5b038d"];
+
+    return labelDays;
+}
+
+//Временной интервал-----------------------------------------------
+- (UILabel*)labelTimeInterval:(NSString*)strigInterval
+{
+    UILabel* labelTimeInterval = [[UILabel alloc] initWithFrame:CGRectMake(208, 5, 100, 15)];
+    labelTimeInterval.text = strigInterval;
+    labelTimeInterval.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];
+    labelTimeInterval.textColor = [UIColor colorWithHexString:@"5b038d"];
+
+    return labelTimeInterval;
+}
+
+//Осталось не изменяемый label-------------------------------------
+- (UILabel*)labelLineLeft
+{
+    UILabel* labelLine = [[UILabel alloc] initWithFrame:CGRectMake(150, 30, 60, 12)];
+    labelLine.text = @"Осталось:";
+    labelLine.font = [UIFont fontWithName:@"HelveticaNeue" size:12];
+    labelLine.alpha = 0.5f;
+
+    return labelLine;
+}
+
+//Оставшееся время выполнения заказа--------------------------------
+- (UILabel*)labelTimeRemaining:(NSString*)remainingString
+{
+    UILabel* labelTime = [[UILabel alloc] initWithFrame:CGRectMake(215, 30, 80, 12)];
+    labelTime.text = remainingString;
+    labelTime.font = [UIFont fontWithName:@"HelveticaNeue" size:12];
+    labelTime.alpha = 0.5f;
+
+    return labelTime;
+}
+
+//Формирование заказа----------------------------------------------
+- (UILabel*)labelFormation:(NSString*)stringFormation
+{
+    
+    UILabel* labelWithFormation = [[UILabel alloc] initWithFrame:CGRectMake(10, 35, 120, 14)];
+    labelWithFormation.text = stringFormation;
+    labelWithFormation.font = [UIFont fontWithName:@"Helvetica-Bold" size:12];
+    labelWithFormation.textColor = [UIColor colorWithHexString:@"2c6530"];
+
+    return labelWithFormation;
+}
 
 @end
