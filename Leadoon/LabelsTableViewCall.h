f60c8df9631ec62ad08b62525562c3df747e3c10
Viktor Mishustin <mrgyugo@gmail.com>
2015-10-15 21:16:34 +0300

    Разработка интерфейса ячейки в "Табло заказов"

---
+++ Leadoon/LabelsTableViewCall.h
@@ -0,0 +1,13 @@
+//
+//  LabelsTableViewCall.h
+//  Leadoon
+//
+//  Created by Viktor on 15.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LabelsTableViewCall : NSObject
+
+@end
---
+++ Leadoon/LabelsTableViewCall.m
@@ -0,0 +1,13 @@
+//
+//  LabelsTableViewCall.m
+//  Leadoon
+//
+//  Created by Viktor on 15.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import "LabelsTableViewCall.h"
+
+@implementation LabelsTableViewCall
+
+@end
