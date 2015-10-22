b8cd8f07c22493acd4bc814505c57be13f13c4e3
Viktor Mishustin <mrgyugo@gmail.com>
2015-10-22 23:13:09 +0300

    Продолжаю работать с картами

---
+++ Leadoon/MapViewScoreboardOrders.h
@@ -0,0 +1,13 @@
+//
+//  MapViewScoreboardOrders.h
+//  Leadoon
+//
+//  Created by Viktor on 22.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewScoreboardOrders : UIViewController
+
+@end
---
+++ Leadoon/MapViewScoreboardOrders.m
@@ -0,0 +1,13 @@
+//
+//  MapViewScoreboardOrders.m
+//  Leadoon
+//
+//  Created by Viktor on 22.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import "MapViewScoreboardOrders.h"
+
+@implementation MapViewScoreboardOrders
+
+@end
