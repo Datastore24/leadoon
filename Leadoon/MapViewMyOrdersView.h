245549e28d9159173dc9bf7140ea2cc178892f4e
Viktor Mishustin <mrgyugo@gmail.com>
2015-10-31 22:13:21 +0300

    Создание нового класса MapViewMyOrdersView и реализации аннотаций на нем

---
+++ Leadoon/MapViewMyOrdersView.h
@@ -0,0 +1,13 @@
+//
+//  MapViewMyOrdersView.h
+//  Leadoon
+//
+//  Created by Viktor on 31.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewMyOrdersView : UIViewController
+
+@end
---
+++ Leadoon/MapViewMyOrdersView.m
@@ -0,0 +1,13 @@
+//
+//  MapViewMyOrdersView.m
+//  Leadoon
+//
+//  Created by Viktor on 31.10.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import "MapViewMyOrdersView.h"
+
+@implementation MapViewMyOrdersView
+
+@end
