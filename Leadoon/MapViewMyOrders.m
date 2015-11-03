efa1629651af2bd90dceec8603ed41d511b6bab3
Viktor Mishustin <mrgyugo@gmail.com>
2015-11-03 12:01:23 +0300

    Работа с картой добавление нового класса

---
+++ Leadoon/MapViewMyOrders.m
@@ -0,0 +1,22 @@
+//
+//  MapViewMyOrders.m
+//  Leadoon
+//
+//  Created by Viktor on 03.11.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import "MapViewMyOrders.h"
+
+@interface MapViewMyOrders ()
+
+@property (weak, nonatomic) IBOutlet UIView *topBarMapViewMyOrders;
+
+
+@end
+
+
+@implementation MapViewMyOrders
+
+
+@end
