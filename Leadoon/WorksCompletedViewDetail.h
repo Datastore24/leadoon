321f449d2fc535115c9a5fb10e7b432f2afdf8bc
Viktor Mishustin <mrgyugo@gmail.com>
2015-11-11 22:00:49 +0300

    Создание классов Выполенные заказы и Детали к ним (Голый интерфейс)

---
+++ Leadoon/WorksCompletedViewDetail.h
@@ -0,0 +1,13 @@
+//
+//  WorksCompletedViewDetail.h
+//  Leadoon
+//
+//  Created by Viktor on 11.11.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorksCompletedViewDetail : UIViewController
+
+@end
