0839a81c52e6b3fad2b6a8c8ec1e4ccd0fc803a4
datastore24 <co@datastore24.ru>
2016-01-04 18:34:53 +0300

    New Relise

--- Pods/SCLAlertView-Objective-C/SCLAlertView/SCLAlertView.m
+++ Pods/SCLAlertView-Objective-C/SCLAlertView/SCLAlertView.m
@@ -230,6 +230,7 @@ SCLTimerDisplay *buttonTimer;
     {
         _viewText.textContainerInset = UIEdgeInsetsZero;
         _viewText.textContainer.lineFragmentPadding = 0;
+        self.automaticallyAdjustsScrollViewInsets = NO;
     }
     
     // Colors
@@ -763,6 +764,11 @@ SCLTimerDisplay *buttonTimer;
             viewColor = UIColorFromHEX(0x6c125d);
             break;
             
+        case Question:
+            viewColor = UIColorFromHEX(0x727375);
+            iconImage = SCLAlertViewStyleKit.imageOfQuestion;
+            break;
+            
         case Custom:
             viewColor = color;
             iconImage = image;
@@ -806,45 +812,19 @@ SCLTimerDisplay *buttonTimer;
         
         // Adjust text view size, if necessary
         CGSize sz = CGSizeMake(_windowWidth - 24.0f, CGFLOAT_MAX);
-        NSDictionary *attr = @{NSFontAttributeName:self.viewText.font};
         
-        if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
+        CGSize size = [_viewText sizeThatFits:sz];
+        
+        CGFloat ht = ceilf(size.height);
+        if (ht < _subTitleHeight)
         {
-            CGRect r = CGRectNull;
-            if(_attributedFormatBlock == nil) {
-                NSString *str = subTitle;
-                r = [str boundingRectWithSize:sz options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:attr context:nil];
-            } else {
-                r = [_viewText.attributedText boundingRectWithSize:sz options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
-            }
-            
-            CGFloat ht = ceilf(r.size.height);
-            if (ht < _subTitleHeight)
-            {
-                self.windowHeight -= (_subTitleHeight - ht);
-                self.subTitleHeight = ht;
-            }
-            else
-            {
-                self.windowHeight += (ht - _subTitleHeight);
-                self.subTitleHeight = ht;
-            }
+            self.windowHeight -= (_subTitleHeight - ht);
+            self.subTitleHeight = ht;
         }
         else
         {
-            NSAttributedString *str =[[NSAttributedString alloc] initWithString:subTitle attributes:attr];
-            CGRect r = [str boundingRectWithSize:sz options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
-            CGFloat ht = ceilf(r.size.height) + 10.0f;
-            if (ht < _subTitleHeight)
-            {
-                self.windowHeight -= (_subTitleHeight - ht);
-                self.subTitleHeight = ht;
-            }
-            else
-            {
-                self.windowHeight += (ht - _subTitleHeight);
-                self.subTitleHeight = ht;
-            }
+            self.windowHeight += (ht - _subTitleHeight);
+            self.subTitleHeight = ht;
         }
         _viewText.frame = CGRectMake(12.0f, _subTitleY, _windowWidth - 24.0f, _subTitleHeight);
     }
@@ -1003,6 +983,12 @@ SCLTimerDisplay *buttonTimer;
     [self showTitle:vc image:nil color:nil title:title subTitle:subTitle duration:duration completeText:closeButtonTitle style:Waiting];
 }
 
+- (void)showQuestion:(UIViewController *)vc title:(NSString *)title subTitle:(NSString *)subTitle closeButtonTitle:(NSString *)closeButtonTitle duration:(NSTimeInterval)duration
+{
+    [self showTitle:vc image:nil color:nil title:title subTitle:subTitle duration:duration completeText:closeButtonTitle style:Question];
+}
+
+
 #pragma mark - Show using new window
 
 - (void)showSuccess:(NSString *)title subTitle:(NSString *)subTitle closeButtonTitle:(NSString *)closeButtonTitle duration:(NSTimeInterval)duration
@@ -1051,6 +1037,11 @@ SCLTimerDisplay *buttonTimer;
     [self showTitle:nil image:nil color:nil title:title subTitle:subTitle duration:duration completeText:closeButtonTitle style:Waiting];
 }
 
+- (void)showQuestion:(NSString *)title subTitle:(NSString *)subTitle closeButtonTitle:(NSString *)closeButtonTitle duration:(NSTimeInterval)duration
+{
+    [self showTitle:nil image:nil color:nil title:title subTitle:subTitle duration:duration completeText:closeButtonTitle style:Question];
+}
+
 #pragma mark - Visibility
 
 - (void)removeTopCircle
@@ -1071,7 +1062,7 @@ SCLTimerDisplay *buttonTimer;
 
 - (CGRect)mainScreenFrame
 {
-    return [self isAppExtension] ? _extensionBounds : [UIScreen mainScreen].bounds;
+    return [self isAppExtension] ? _extensionBounds : [UIApplication sharedApplication].keyWindow.bounds;
 }
 
 - (BOOL)isAppExtension
